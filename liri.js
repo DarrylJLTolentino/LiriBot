3f2e0bae9d3fbb903c8698b61002f19b63dd289d
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-06-19 18:09:20 -0700

    Adding Continue and ConcertThis function within liri.js

--- liri.js
+++ liri.js
@@ -15,8 +15,8 @@ function ConcertThis() {
             default: "Glitch Mob"
         }
     ]).then(function (response) {
-        axios.get("https://rest.bandsintown.com/artists/" + response.artist + "/events?app_id=codingbootcamp").then(
-            function (response) {
+        axios.get("https://rest.bandsintown.com/artists/" + response.artist + "/events?app_id=codingbootcamp")
+            .then(function (response) {
                 // console.log(response.data);
                 if (response.data) {
                     for (var i = 0; i < response.data.length; i++) {
@@ -35,13 +35,54 @@ function ConcertThis() {
                         }
                         console.log(str + "\nDate of the Event: " + moment(response.data[i].datetime).format('LLL'));
                     }
+                    Continue();
                 }
-                if(response.data.length < 1) {
+                if (response.data.length < 1) {
                     console.log("Rrrrrgh...this isn't working! There are no upcoming concerts for " + response.artist + ".");
+                    Continue();
                 }
-            }
-        )
+            }).catch(function (error) {
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    console.log("---------------Data---------------");
+                    console.log(error.response.data);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.status);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.headers);
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                    console.log(error.request);
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log("Error", error.message);
+                }
+                console.log(error.config);
+                Continue();
+            });
     });
+}
+
+
+
+function Continue() {
+    inquirer.prompt([
+        {
+            type: "confirm",
+            name: "continue",
+            message: "Would you like to continue traveller?",
+            default: true
+        }
+    ]).then(function (response) {
+        if (response.continue) {
+            QuestionTheUser();
+        }
+        else {
+            console.log("Good luck Traveller! Turning off the optics... they can't see me...");
+        }
+    })
 }
 
 function QuestionTheUser() {
